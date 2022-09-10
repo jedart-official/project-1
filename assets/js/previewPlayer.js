af0539dfebffdb7a82fee2cb76d26fd94b299456
jedart-official <mhaker2004@gmail.com>
2022-09-10 23:55:52 +0600

    add css animations and music player

---
+++ assets/js/previewPlayer.js
@@ -0,0 +1,98 @@
+const player = document.querySelector(".preview__player")
+const playerBtn = document.querySelector(".preview__play")
+const playerAudio = document.querySelector(".preview__audio")
+const playerContainer = document.querySelector(".preview__line")
+const playerBar = document.querySelector(".fully__line")
+const playerTime = document.querySelector(".preview__time")
+const playerCurrentTime = document.querySelector(".preview__current")
+const playerDurationTime = document.querySelector(".preview__duration")
+const playerWrapper = document.querySelector(".preview__wrapper")
+
+
+
+// play
+
+function play(){
+    playerAudio.play()
+}
+
+// pause 
+
+function pause(){
+    playerAudio.pause()
+}
+
+// pause / play
+
+playerBtn.addEventListener("click", () =>{
+    if (playerBtn.classList.contains("preview__pause")){
+        playerAudio.pause()
+        playerBtn.classList.remove("preview__pause")
+    }
+    else {
+        play()
+        playerBtn.classList.add("preview__pause")
+    }
+})
+
+// update progressBar
+
+function progressBarUpdate(e){
+    const {duration, currentTime} = e.srcElement
+    const playerDuration = (currentTime  /  duration) * 100
+    playerBar.style.width = `${playerDuration}%`
+    newcurrentTime = Math.round(currentTime, 1)
+    currentMin = Math.floor(newcurrentTime / 60)
+    currentSec = Math.floor(newcurrentTime % 60)
+    newcurrentTimeFormat = [
+        currentMin.toString().padStart(2, '0'),
+        currentSec.toString().padStart(2, '0')
+    ].join(":")
+    playerCurrentTime.innerHTML = `${newcurrentTimeFormat} -`
+    
+}
+
+playerAudio.addEventListener("timeupdate", progressBarUpdate)
+
+
+// set progress
+
+function progressSet(e){
+    const allWidth = this.clientWidth
+    const setWidth = e.offsetX
+    const audioDuration = playerAudio.duration
+    playerAudio.currentTime = (setWidth / allWidth) * audioDuration 
+    console.log(playerAudio.currentTime);
+
+}
+playerContainer.addEventListener("click", progressSet)
+
+// Autoplay 
+
+function autoplay(){
+    play()
+}
+
+playerAudio.addEventListener("ended", autoplay)
+
+
+function moveSetUpdate(e){
+    const moveAllWidth = playerContainer.clientWidth
+    const moveSetWidth = e.offsetX
+    const audioDuration = playerAudio.duration
+    playerAudio.currentTime = (moveSetWidth / moveAllWidth) * audioDuration
+    playerBar.style.width = `${moveSetWidth}px`
+}
+function moveSet(e){
+    playerWrapper.addEventListener('mousemove', moveSetUpdate);
+}
+
+
+
+function moveRemove(){
+    playerWrapper.removeEventListener("mousemove", moveSetUpdate)
+    
+}
+
+playerBar.addEventListener("mousedown", moveSet)
+playerWrapper.addEventListener("mouseup", moveRemove)
