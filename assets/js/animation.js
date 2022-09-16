ba13961658bdaeaf926e18a28ed9e495c930370d
JeDaRT <85687499+jedart-official@users.noreply.github.com>
2022-09-16 16:05:22 +0600

    finished project v2

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,4 +1,6 @@
-const sectionMedia = document.querySelector(".album")
+const sectionAlbum = document.querySelector(".album")
+const sectionMusic = document.querySelector(".nextmedia")
+const sectionMedia = document.querySelector(".media")
 const transformItem = document.querySelector(".image__transform")
 
 function elementInViewport(el){
@@ -11,8 +13,9 @@ function elementInViewport(el){
     );
 }
 
-document.addEventListener("scroll", (e)=> {
-    let viewMedia = elementInViewport(sectionMedia)
+
+function checkAlbum(){
+    let viewMedia = elementInViewport(sectionAlbum)
     let points = document.querySelectorAll(".album__point")
     let albumTitle = document.querySelector(".album__title")
     let albumText = document.querySelector(".album__text")
@@ -32,4 +35,53 @@ document.addEventListener("scroll", (e)=> {
         albumText.style.opacity = "0"
         albumTitle.style.animation = "none"
     }
+}
+
+function checkMedia(){
+    let viewMedia = elementInViewport(sectionMedia)
+    let leftBlock = document.querySelector(".media__content")
+    let rightBlock = document.querySelector(".media__image")
+    console.log(window);
+    if (window.innerWidth > 1200){
+        if (viewMedia == true){
+        
+            leftBlock.style.transform = "translateX(0)"
+            leftBlock.style.opacity = "1"
+            rightBlock.style.transform = "translate(0, -50%)"
+            rightBlock.style.opacity = "1"
+        }
+        else {
+            leftBlock.style.transform = "translateX(-800px)"
+            leftBlock.style.opacity = "0"
+            rightBlock.style.transform = "translate(1000px, -50%)"
+            rightBlock.style.opacity = "0"
+        }
+    }
+    
+}
+
+function checkMusic(){
+    let viewMedia = elementInViewport(sectionMusic)
+    let leftBlock = document.querySelector(".nextmedia__left")
+    let rightBlock = document.querySelector(".nextmedia__right")
+    if (viewMedia == true){
+        leftBlock.style.transform = "translateX(0)"
+        leftBlock.style.opacity = "1"
+        rightBlock.style.transform = "translateX(0)"
+        rightBlock.style.opacity = "1"
+    }
+    else {
+        leftBlock.style.transform = "translateX(-800px)"
+        leftBlock.style.opacity = "0"
+        rightBlock.style.transform = "translateX(800px)"
+        rightBlock.style.opacity = "0"
+    }
+}
+
+
+
+document.addEventListener("scroll", (e)=> {
+    checkAlbum()
+    checkMedia()
+    checkMusic()
 })
